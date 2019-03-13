96759ce52e3803567635fc3660ec180399c2d02b
AndyMoreHaul <47096371+AndyMoreHaul@users.noreply.github.com>
2019-03-13 01:12:24 -0400

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
+
+    private int count;
+
     public float speed;
+    public Text countText;
+    public Text winText;
+
     public float jumpForce;
 
+    // float values for values of controller
     // Start is called before the first frame update
     void Start()
     {
 
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+
 
     }
 
@@ -21,7 +33,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey("escape"))
             Application.Quit();
-    }
+    } // Escape key for game build
 
     void FixedUpdate()
     {
@@ -30,7 +42,7 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2(moveHorizontal, 0);
 
         rb2d.AddForce(movement * speed);
-    }
+    } // movement of player
 
     void OnCollisionStay2D(Collision2D collision)
     {
@@ -41,6 +53,29 @@ public class PlayerController : MonoBehaviour
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
 
             }
+        }
+    } // ground collision
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText();
+        }
+    } //collider for pickup and count
+        
+         void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 4)
+        {
+         
+        winText.text = "You Win!";
+            {
+            
+            }
+           
         }
     }
 }
